f491c166ca18eb4aca6b0e095d1e3633811f9899
James Feore <jjfeore@gmail.com>
2017-04-19 09:16:24 -0700

    Reviewed, removed comments, updated the pull request template

--- PULL_REQUEST_TEMPLATE.md
+++ PULL_REQUEST_TEMPLATE.md
@@ -2,14 +2,12 @@
 **Today, _ and _ paired together. It took about _**
 
 #### Reflect and summarize on your process for each `TODO` item :  
-  1. First, we started on ...
-  2. Next, we ...
-  3. 
+  1. First, Jeff basically did everything.
+  2. Next, I reviewed the work he completed to ensure comprehension.
+  3. I removed the comments and pushed things back up. Still a little unsure of the use/purpose of class-level functions, but I understand them in principle.
 
 #### Checklist (before submitting, fill in each set of square brackets with an 'x')
-- [] We have titled the Pull Request similar to our branch name (ex: 'brian-rick'). 
-- [] This PR includes commits from both myself and my partner; e.g. We followed good pair programming practices by switching driver/navigator roles.
-- [] There is no extraneous, unrelated code included in this PR.
-- [] We have summarized our `TODO:` process above.
-
-
+- [x] We have titled the Pull Request similar to our branch name (ex: 'brian-rick').
+- [x] This PR includes commits from both myself and my partner; e.g. We followed good pair programming practices by switching driver/navigator roles.
+- [x] There is no extraneous, unrelated code included in this PR.
+- [x] We have summarized our `TODO:` process above.
--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -9,11 +9,6 @@ function Article (rawDataObj) {
   this.publishedOn = rawDataObj.publishedOn;
 }
 
-// REVIEW: Instead of a global `articles = []` array, let's track this list of all articles directly on the
-// constructor function. Note: it is NOT on the prototype. In JavaScript, functions are themselves
-// objects, which means we can add properties/values to them at any time. In this case, we have
-// a key/value pair to track, that relates to ALL of the Article objects, so it does not belong on
-// the prototype, as that would only be relevant to a single instantiated Article.
 Article.all = [];
 
 Article.prototype.toHtml = function() {
@@ -26,13 +21,6 @@ Article.prototype.toHtml = function() {
   return template(this);
 };
 
-// REVIEW: There are some other functions that also relate to articles across the board, rather than
-// just single instances. Object-oriented programming would call these "class-level" functions,
-// that are relevant to the entire "class" of objects that are Articles.
-
-// REVIEW: This function will take the rawData, however it is provided,
-// and use it to instantiate all the articles. This code is moved from elsewhere, and
-// encapsulated in a simply-named function for clarity.
 Article.loadAll = function(rawData) {
   rawData.sort(function(a,b) {
     return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
@@ -43,22 +31,11 @@ Article.loadAll = function(rawData) {
   })
 }
 
-// This function will retrieve the data from either a local or remote source,
-// and process it, then hand off control to the View.
 Article.fetchAll = function() {
   if (localStorage.rawData) {
-    // When rawData is already in localStorage,
-    // we can load it with the .loadAll function above,
-    // and then render the index page (using the proper method on the articleView object).
-    Article.loadAll(JSON.parse(localStorage.getItem('rawData'))); //TODO: What do we pass in to loadAll()?
-    //TODO: What method do we call to render the index page?
+    Article.loadAll(JSON.parse(localStorage.getItem('rawData')));
     articleView.initIndexPage();
   } else {
-    // TODO: When we don't already have the rawData,
-    // we need to retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
-    // cache it in localStorage so we can skip the server call next time,
-    // then load all the data into Article.all with the .loadAll function above,
-    // and then render the index page.
     $.getJSON('data/hackerIpsum.json').done(function (rawData) {
       Article.loadAll(rawData);
       localStorage.setItem('rawData', JSON.stringify(rawData));
